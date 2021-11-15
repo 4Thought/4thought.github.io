3a57822ed0c71e9566793ca3018a73fa814871e1
Wolfgang <wolf@okwolf.com>
2021-11-15 15:20:35 -0800

    menu toggle script

--- dist/scripts/includer.js
+++ dist/scripts/includer.js
@@ -1,3 +1,18 @@
+function setupMenuToggle() {
+  var menuToggleInterval = setInterval(function () {
+    document
+      .querySelectorAll("[data-nav-drawer-toggle]")
+      .forEach(function (navToggle) {
+        clearInterval(menuToggleInterval);
+        navToggle.addEventListener("click", function () {
+          document
+            .querySelector("[data-nav-drawer]")
+            .classList.toggle("is-active");
+        });
+      });
+  }, 100);
+}
+
 window.onload = function () {
   function a(a, b) {
     var c = /^(?:file):/,
@@ -18,4 +33,6 @@ window.onload = function () {
     c[b].hasAttribute &&
       c[b].hasAttribute("data-include") &&
       a(c[b], c[b].getAttribute("data-include"));
+
+  setupMenuToggle();
 };
